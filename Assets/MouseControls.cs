93095d4d310be0f3133f7377fe9c3b708e537458
Timmy Chan <fancygamer123@gmail.com>
2017-12-20 23:57:14 +0100

    addde simple mouseControls and outline for tiles when clicked

---
+++ Assets/MouseControls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControls : MonoBehaviour {
+    //Control settings
+    Camera cam;
+    KeyCode primaryMouseButton = KeyCode.Mouse0;
+    cakeslice.Outline currentOutlinedComponent;
+	// Use this for initialization
+	void Start () {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(primaryMouseButton)) {
+            print("pressed");
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit)) {
+                Transform objectHit = hit.transform;
+                cakeslice.Outline c = objectHit.GetComponent<cakeslice.Outline>();
+                if(c) {
+                    c.eraseRenderer = false;
+                    if(currentOutlinedComponent) {
+                        currentOutlinedComponent.eraseRenderer = true;
+                    }
+                    currentOutlinedComponent = c;
+                }
+            }
+        }
+	}
+}
