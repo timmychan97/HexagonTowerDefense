24ef8b528ed29b23e7f4990de5bd3149b9f8c2c3
Timmy Chan <fancygamer123@gmail.com>
2017-12-20 23:24:25 +0100

    Added tower and mountain tile model. Imported outline assets. New CamController script. TileGenerator

---
+++ Assets/CamController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour {
+    Vector3 targetVelocity = new Vector3(0, 0, 0);
+    Vector3 velocity = new Vector3(0, 0, 0);
+    // Use this for initialization
+    float maxSpeed = 5f;
+
+    /*Ease Properties:*/
+    float easeDuration = 3f;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W)) {
+            print("W");
+            targetVelocity += new Vector3(0, 0, 2f);
+        }
+        if(Input.GetKey(KeyCode.S)) {
+            targetVelocity -= new Vector3(0, 0, 2f);
+        }
+        if(Input.GetKey(KeyCode.A)) {
+            targetVelocity -= new Vector3(2f, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.D)) {
+            targetVelocity += new Vector3(2f, 0, 0);
+        }
+        print("f" + targetVelocity);
+        //move the targetVelocity slowly to the middle/zero
+        if(targetVelocity.magnitude == 0.01f)
+            targetVelocity = Vector3.zero;
+        else
+            targetVelocity = (targetVelocity-Vector3.zero)*0.3f; //t is [0,1]
+        targetVelocity = Vector3.ClampMagnitude(targetVelocity, maxSpeed); //limits the speed below maxSpeed
+        print(targetVelocity);
+        //move the real velocity against the targetVelocity
+        velocity = Vector3.Lerp(velocity,targetVelocity, 1f);
+        if (velocity.magnitude <= 0.01f && targetVelocity == Vector3.zero) {
+            velocity = Vector3.zero;
+        }
+        transform.position += velocity;
+	}
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+    public GameObject tile;
+    // 10 = x distance between hexagons
+    // Use this for initialization
+    private float x = 0f;
+    private float z = 0f;
+    private int xTiles = 30;
+    private int zTiles = 18;
+    void Start () {
+        for (int i = 0; i < xTiles; i++) {
+            for(int j = 0; j < zTiles; j++) {
+                GameObject t = Instantiate(tile, this.transform);
+                t.transform.position = new Vector3(x, t.transform.position.y, z);
+                z += 8.66025403784f; //2 * sqrt(pow(5,2)+pow(2.5,2)) = y distance between hexagons
+            }
+            x += 5;
+            z = (i % 2 == 0) ? 4.33012701892f : 0f;
+        }
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
